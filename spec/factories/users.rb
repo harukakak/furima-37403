934edd7f39b2603cd7d03c15dc1622a85c5cf2d0
harukakak <87980271+harukakak@users.noreply.github.com>
2022-01-24 19:05:14 +0900

    Rubocop

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nick_name             {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    last_name             {Faker::Name.last_name}
-    first_name            {Faker::Name.first_name}
-    last_name_kana        {Faker::Name.initials(number: 2)}
-    first_name_kana       {Faker::Name.initials(number: 2)}
-    birthday              {Faker::Date.birthday(min_age: 18, max_age: 65)}
+    nick_name             { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name             { Faker::Name.last_name }
+    first_name            { Faker::Name.first_name }
+    last_name_kana        { Faker::Name.initials(number: 2) }
+    first_name_kana       { Faker::Name.initials(number: 2) }
+    birthday              { Faker::Date.birthday(min_age: 18, max_age: 65) }
   end
 end
