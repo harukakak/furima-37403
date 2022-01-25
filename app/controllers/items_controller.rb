86817fc2eb006ed0732afdc65e3b14068fee188e
harukakak <87980271+harukakak@users.noreply.github.com>
2022-01-25 19:43:59 +0900

    ActiveHashを使って新規登録画面の作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,25 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.includes(:user).order("created_at DESC")
   end
 
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title,:content,:category_id,:status_id,:charge_id,:area_id,:term_id,:price,:user)
+  end
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+  belongs_to :area
+  belongs_to :charge
+  belongs_to :status
+  belongs_to :term
+  belongs_to :user
+
+  validates :title, :content, :category_id, :status_id, :charge_id, :area_id, :term_id, :price, :user, presence: true
+  validates :genre_id, :area_id, :term_id, :category_id, :status_id, :charge_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
