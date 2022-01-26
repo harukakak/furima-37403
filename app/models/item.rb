57a67f01ec1096153bb0a10c7a35fe61be300daa
harukakak <87980271+harukakak@users.noreply.github.com>
2022-01-26 20:46:33 +0900

    修正1

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,8 +8,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                    format: { with: /\A[0-9]+\z/ }
-  validates :title, :content, :category_id, :status_id, :charge_id, :area_id, :term_id, :price, :user, :image, presence: true
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :title, :content, :category_id, :status_id, :charge_id, :area_id, :term_id, :price, :image, presence: true
   validates :area_id, :term_id, :category_id, :status_id, :charge_id, numericality: { other_than: 1, message: "can't be blank" }
 end
