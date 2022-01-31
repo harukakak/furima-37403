129ddf736cf616aa83c3892def72032b336b1ba7
harukakak <87980271+harukakak@users.noreply.github.com>
2022-01-31 17:22:22 +0900

    test

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -21,7 +21,7 @@ const pay = () => {
         const tokenObj = `<input value=${token} name='token' type="hidden"> `;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
-      console.log(card)
+      console.log(status)
 
       document.getElementById("card-number").removeAttribute("name");
       document.getElementById("card-cvc").removeAttribute("name");
